52eeec5ca34f2e2f873315aae93bfa518e919762
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2019-12-29 12:45:40 -0300

    Criação de classes iniciais

---
+++ src/entities/Account.java
@@ -0,0 +1,53 @@
+package entities;
+
+public class Account {
+	
+	private Integer number;
+	private String holder;
+	private double balance;
+	private double withdrawLimit;
+	
+	public Account(Integer number, String holder, double balance, double withdrawLimit) {
+		super();
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+		
+	
+
+}
